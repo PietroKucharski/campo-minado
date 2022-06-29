c20825a9310e9d2289e5fe8b244e03aaa94632c6
Pietro Santos Kucharski Pereira Lima <pskplima@furb.br>
2022-06-29 19:32:13 -0300

    subindo arquivos

--- src/moduleP/GameP.java
+++ src/moduleP/GameP.java
@@ -1,13 +1,79 @@
-
+package moduleP;
 
 import modelo.Tabuleiro;
 import visao.TabuleiroConsole;
+
+import javax.swing.JPanel;
+
 import gameShared.Game;
+import gameShared.Player;
 
 public class GameP implements Game {
 	public static void main(String[] args) {
 
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		new TabuleiroConsole(tabuleiro);
+		
+	}
+
+	@Override
+	public int getActivations() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Player getBestPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getBestPlayerPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHowManyPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JPanel getPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player getWorstPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWorstPlayerPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void start(Player p) {
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+		
 	}
 }
